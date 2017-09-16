594bfa1a9835f156652c569246714bd5e91ec43e
ccljason <jason.c.leung@gmail.com>
2017-09-15 23:01:11 -0700

    Selected to add?

---
+++ Misc1/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc1
+{
+    public static class RunThis
+    {
+      public static void Example()
+      {
+
+      }
+    }
+}
